5c8d707d7087013ce47d7af0152d5cbca7c4a8a8
Colin McDonald <colinmcd0731@gmail.com>
2021-06-10 12:05:31 -0400

    Add comments to Main.hs

--- Main.hs
+++ Main.hs
@@ -8,17 +8,13 @@ import Compile
 import Util
 import System.Exit
 
---testTerm = "\\? x : bool ->1 bool . x y (observe abc <- \\ y : unit. y y) (if true then false else true)"
---lexedTerm = Lexer.lexStr testTerm
---parsedTerm = lexedTerm >>= Parser.parseTerm
 
+-- Parse a file, check and elaborate it, then compile to FGG and output it
 main :: IO ()
 main =
   getContents >>= \ s ->
   let lexed = Lex.lexStr s
       parsed = lexed >>= Parse.parseFile in
-    --putStrLn (show lexed) >>
-    --putStrLn (show parsed) >>= \ _ -> -- (>>) won't parse with ($)
     maybe2 parsed (putStrLn "Parse error") $ \ ps ->
     either die (\ a -> a >> exitSuccess) $ checkFile ps >>= \ ps' ->
     Right $ putStrLn $ show $ file2fgg ps'
