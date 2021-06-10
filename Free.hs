2228b80f986fba58196fc20706adb33d8dcaf9fe
Colin McDonald <colinmcd0731@gmail.com>
2021-06-10 10:53:38 -0400

    Add comments to Free.hs

--- Free.hs
+++ Free.hs
@@ -3,6 +3,23 @@ import Exprs
 import Ctxt
 import qualified Data.Map as Map
 
+-- For checking linearity, vars can appear:
+-- LinNo: not at all
+-- LinYes: just once
+-- LinErr: multiple times
+data Lin = LinNo | LinYes | LinErr
+  deriving Eq
+
+linIf :: Lin -> a -> a -> a -> a
+linIf LinYes y n e = y
+linIf LinNo  y n e = n
+linIf LinErr y n e = e
+
+linIf' :: Lin -> Lin -> Lin -> Lin
+linIf' i y n = linIf i y n LinErr
+
+
+-- Returns a map of the free vars in a term, with the max number of occurrences
 freeVars :: UsTm -> Map.Map Var Int
 freeVars (UsVar x) = Map.singleton x 1
 freeVars (UsLam x tp tm) = Map.delete x $ freeVars tm
@@ -23,26 +40,19 @@ freeVarsCase (Case c xs tm) = foldr Map.delete (freeVars tm) xs-}
 freeVarsCase :: CaseUs -> Map.Map Var Int
 freeVarsCase (CaseUs c xs tm) = foldr Map.delete (freeVars tm) xs
 
+-- Returns the (max) number of occurrences of x in tm
 freeOccurrences :: Var -> UsTm -> Int
 freeOccurrences x tm = Map.findWithDefault 0 x (freeVars tm)
 
+-- Returns if x appears free in tm
 isFree :: Var -> UsTm -> Bool
 isFree x tm = Map.member x (freeVars tm)
 
+-- Returns if x occurs at most once in tm
 isAff :: Var -> UsTm -> Bool
 isAff x tm = freeOccurrences x tm <= 1
 
-data Lin = LinNo | LinYes | LinErr
-  deriving Eq
-
-linIf :: Lin -> a -> a -> a -> a
-linIf LinYes y n e = y
-linIf LinNo  y n e = n
-linIf LinErr y n e = e
-
-linIf' :: Lin -> Lin -> Lin -> Lin
-linIf' i y n = linIf i y n LinErr
-
+-- Returns if x appears exactly once in tm
 isLin :: Var -> UsTm -> Bool
 isLin x tm = h tm == LinYes where
   linCase :: CaseUs -> Lin
@@ -61,6 +71,11 @@ isLin x tm = h tm == LinYes where
   h (UsSamp d y) = LinNo
 
 
+{- ====== Alpha-Renaming Functions ====== -}
+-- These function rename all bound variables
+-- to unique names that don't occur anywhere
+-- else in the entire program.
+
 type VarMap = Map.Map Var Var
 newtype RenameM a = RenameM (VarMap -> (a, VarMap))
 instance Functor RenameM where
@@ -78,18 +93,22 @@ instance Monad RenameM where
         (RenameM fb) = g a in
       fb xs'
 
+-- Lookup x in the renaming map
 getVar :: Var -> RenameM Var
 getVar x = RenameM $ \ xs -> (Map.findWithDefault x x xs, xs)
 
+-- Add x->x to the renaming map
 bindVar :: Var -> RenameM a -> RenameM a
 bindVar x (RenameM fa) = RenameM $ \ xs ->
   let x' = Map.findWithDefault x x xs
       (a, xs') = fa xs in
     (a, Map.insert x x' xs')
-    
+
+-- Bind a list of vars (c.f. bindVar)
 bindVars :: [Var] -> RenameM a -> RenameM a
 bindVars = flip (foldr bindVar)
 
+-- Pick a new name, if necessary
 newVar :: Var -> RenameM Var
 newVar x = RenameM $ \ xs ->
   let x' = newVarH xs x in
@@ -102,6 +121,7 @@ newVar x = RenameM $ \ xs ->
         else x'
   newVarH xs x = if Map.member x xs then h xs x 1 else x
 
+-- Alpha-rename a user-term
 renameTerm :: UsTm -> RenameM UsTm
 renameTerm (UsVar x) =
   pure UsVar <*> getVar x
@@ -116,6 +136,7 @@ renameTerm (UsCase tm cs) =
 renameTerm (UsSamp d y) =
   pure (UsSamp d) <*> getVar y
 
+-- Alpha-rename a user-case
 renameCase :: CaseUs -> RenameM CaseUs
 renameCase (CaseUs x as tm) =
   bindVars as $
@@ -123,18 +144,22 @@ renameCase (CaseUs x as tm) =
     <*> foldr (\ a as' -> pure (:) <*> newVar a <*> as') (return []) as
     <*> renameTerm tm
 
+-- Alpha-rename a type
 renameType :: Type -> RenameM Type
 renameType (TpVar y) = pure TpVar <*> getVar y
 renameType (TpArr tp1 tp2) = pure TpArr <*> renameType tp1 <*> renameType tp2
 
+-- Alpha-rename a constructor definition
 renameCtor :: Ctor -> RenameM Ctor
 renameCtor (Ctor x tps) = pure (Ctor x) <*> foldr (\ tp tps' -> pure (:) <*> renameType tp <*> tps') (return []) tps
 
+-- Alpha-rename an entire user-program
 renameProgs :: UsProgs -> RenameM UsProgs
 renameProgs (UsProgExec tm) = pure UsProgExec <*> renameTerm tm
 renameProgs (UsProgFun x tp tm ps) = pure (UsProgFun x) <*> renameType tp <*> renameTerm tm <*> renameProgs ps
 renameProgs (UsProgData y cs ps) = pure (UsProgData y) <*> foldr (\ c cs' -> pure (:) <*> renameCtor c <*> cs') (return []) cs <*> renameProgs ps
 
+-- Alpha-rename a file
 alphaRename :: Ctxt -> UsProgs -> UsProgs
 alphaRename g ps =
   let xs = Map.mapWithKey const g
