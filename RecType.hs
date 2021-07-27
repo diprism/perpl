acb38c5f1ccec553676feaa553880a47936c29b7
Colin McDonald <colinmcd0731@gmail.com>
2021-07-27 10:01:33 -0400

    Fix disentanglement bug (nested case-ofs didn't work)

--- RecType.hs
+++ RecType.hs
@@ -41,89 +41,6 @@ getRecTypes' g [] = []
 getRecTypes :: Ctxt -> Progs -> [Var]
 getRecTypes g (Progs ds end) = getRecTypes' g ds
 
-
-
-{-findUnfolds :: [Var] -> Progs -> ([(Map.Map Var Type, Term, Cases)], [(Term, Progs)] -> Progs)
-findUnfolds recs (ProgsData x cs ps) = ()
-findUnfolds recs (ProgsFun x tp tm ps) = ()
-findUnfolds recs (ProgsExtern x xp tp ps) = ()
-findUnfolds recs (ProgsExec tm) = ()-}
-
-{-defun :: (Int -> Var) -> Term -> Type -> Type -> Term -> ([Case], Term)
-defun name f y y' tm = let (cs, tm', tp) = h tm in (cs, tm') where
-  h :: Term -> ([Case], Term, Type)
-  h (TmVar x tp sc) = error "TODO"
-  h (TmLam x tp tm tp') = error "TODO"
-  h (TmApp tm1 tm2 tp2 tp) =
-    let (cs1, tm1', TpArr _ tp') = h tm1
-        (cs2, tm2', tp2') = h tm2
-    in
-      (cs1 ++ cs2, TpApp tm1' tm2' tp2' tp', tp')
-  h (TmCase tm tp1 cs tp2)
-    let (cas, tm', tp1') = h tm
-        (ccas, cs', tp2') = foldr (\ (Case x as xtm) (ccas, cs', _) -> let (cca, xtm', xtp) = h xtm in (cca ++ ccas, Case x as xtm', xtp)) ([], [], tp2) cs
-    in
-      if tp1 == y then
-        (cas ++ ccas, TmCase (TmApp f tm' (TpArr y y')) y' cs' tp2')
-      else
-        (cas ++ ccas, TmCase tm' tp1' cs' tp2')
-  h (TmSamp d tp) = error "TODO"
-  h (TmCtor x as tp) =
-    let (cs, as') = foldr (\ (atm, atp) (cs, as) -> let (cs', atm', atp') = h atm in (cs' ++ cs, (atm', atp') : as)) ([], []) as in
-      (cs, TmCtor x as' tp)
-  h (TmFold fuf tm tp)
-    | tp == y && fuf =
-      let (cs, tm', tp') = h tm
-          fvs = Map.toList (freeVars' tm')
-          as = toTermArgs fvs
-          x = name (length cs) in
-        ((Case x fvs tm') : cs, TmCtor x as y', tp')
-    | otherwise =
-      let (cs, tm', tp') = h tm in
-        (cs, TmFold fuf tm' tp, tp')
-
-computeRefun :: Ctxt -> Var -> Type -> Term -> Maybe (Term, Prog)
-computeRefun g f y = h where
-  h :: Term -> Maybe (Term, Prog)
-  h (TmVar x tp sc) = Nothing
-  h (TmLam x tp tm tp') =
-    h tm >>= \ (tm', prog) -> Just (TmLam x tp tm' tp', prog)
-  h (TmApp tm1 tm2 tp2 tp) = error "TODO: what if both have unfold?"
-  h (TmCase tm tp1 cs tp2)
-    | tp1 == y =
-      let fvs = Map.toList (freeVars' (TmCase tm tp1 cs tp2))
-          ptp = joinArrows (map snd fvs) tp2 in
-        Just (joinApps (TmVar f ptp ScopeGlobal) (toTermArgs fvs) tp2,
-              ProgFun f ptp (joinLams fvs (TmCase tm tp1 cs tp2)))
-    | otherwise = error "TODO"
-  h (TmSamp d tp) = Nothing -- TODO: what if tp == y?
-  h (TmCtor x as tp) = error "TODO"
-  h (TmFold fuf tm tp) =
-    h tm >>= \ (tm', prog) -> Just (TmFold fuf tm' tp, prog)
-
-refun :: Term -> Type -> Type -> Term -> Term
-refun f y y' tm = h tm where
-  h :: Term -> Term
-  h (TmVar x tp sc) = error "TODO"
-  h (TmLam x tp tm tp') = error "TODO"
-  h (TmApp tm1 tm2 tp2 tp) = error "TODO"
-  h (TmCase tm tp1 cs tp2) = error "TODO"
-  h (TmSamp d tp) = error "TODO"
-  h (TmCtor x as tp) = error "TODO"
-  h (TmFold fuf tm tp) = error "TODO"
--}
-
-{-data StateM s m = 
-data DisentangleM a = DisentangleM a [([(Var, Type)], [Case])]
-instance Functor Disentangle where
-  fmap f (DisentangleM a cs) = DisentangleM (f a) cs
-instance Applicative DisentangleM where
-  pure a = DisentangleM a []
-  DisentangleM a2b cs <*> DisentangleM a cs' =
-    DisentangleM (a2b a) (cs ++ cs')
-instance Monad DisentangleM where
-  DisentangleM a cs >>= f = let DisentangleM b cs' = f a in -}
-
 -- N.B. state list is in reverse order
 type DisentangleResult = (Map.Map Var Type, Var, Type, [Case], Type)
 type DisentangleState = [DisentangleResult]
@@ -146,13 +63,14 @@ disentangleTerm recs = h where
   h (TmCase tm tp1 cs tp2)
     | tp1 `elem` recs =
       h tm >>= \ tm' ->
+      mapM (\ (Case x as tm) -> pure (Case x as) <*> h tm) cs >>= \ cs' ->
       State.get >>= \ unfolds ->
-      let fvs = freeVars' (TmCase (TmVarG DefVar "" [] tp1) tp1 cs tp2)
+      let fvs = freeVars' (TmCase (TmVarG DefVar "" [] tp1) tp1 cs' tp2)
           fvs' = Map.toList fvs
           --ptp = joinArrows (map snd fvs') tp2
           name = applyName (length unfolds)
           rtm = TmVarG DefVar name ((tm', tp1) : toTermArgs fvs') tp2 in
-        State.put ((fvs, name, tp1, cs, tp2) : unfolds) >>
+        State.put ((fvs, name, tp1, cs', tp2) : unfolds) >>
         pure rtm
     | otherwise =
       pure TmCase <*> h tm <*> pure tp1
