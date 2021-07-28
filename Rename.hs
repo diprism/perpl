bd50ca59b81b50bca4bacfa887a81112b5d92113
Colin McDonald <colinmcd0731@gmail.com>
2021-07-28 13:29:34 -0400

    Better alpha renaming convention

--- Rename.hs
+++ Rename.hs
@@ -42,25 +42,50 @@ bindVar x (RenameM fa) = RenameM $ \ xs ->
 bindVars :: [Var] -> RenameM a -> RenameM a
 bindVars = flip (foldr bindVar)
 
+
+data SplitVar = SplitVar String Int String
+succSplitVar (SplitVar pre i suf) = SplitVar pre (succ i) suf
+instance Show SplitVar where
+  show (SplitVar pre i suf) = pre ++ show i ++ suf
+
+-- Splits abc14'' into ("abc", 14, "\'\'")
+splitVar :: Var -> SplitVar
+splitVar x =
+  let (pre, i, suf) = h True (reverse x)
+      pre' = reverse pre
+      i' = reverse i
+      suf' = reverse suf
+      i'' = if null i' then 0 else succ (read i' :: Int)
+  in
+    SplitVar pre' i'' suf'
+  where
+    h :: Bool -> String -> (String, String, String)
+    h b "" = ("", "", "")
+    h True ('\'' : cs) =
+      let (pre, i, suf) = h True cs in
+        (pre, i, '\'' : suf)
+    h True (c : cs) = h False (c : cs)
+    h False (c : cs)
+      | isDigit c =
+        let (pre, i, suf) = h False cs in
+          (pre, c : i, suf)
+      | otherwise = (c : cs, "", "")
+
+
+
 -- Pick a new name, if necessary
 newVar :: Var -> RenameM Var
 newVar x = RenameM $ \ xs ->
   let x' = newVarH xs x in
     (x', Map.insert x x' (Map.insert x' x' xs))
-  where
-  var2num :: Var -> Integer
-  var2num n = if null n then 0 else read n :: Integer
-  -- Pulls the number suffix from a var, and returns it + 1. Ex: "foo123" -> ("foo", 124)
-  pullNum :: Var -> (Var, Integer)
-  pullNum = fmap succ . either id (\ n -> ("", var2num n)) . foldr (\ c -> either (\ (p, s) -> Left (c : p, s)) (\ n -> if isDigit c then Right (c : n) else Left ([c], var2num n))) (Right "")
-  
-  h xs x n =
-    let x' = x ++ show n in
+  where  
+  h xs x =
+    let x' = show x in
       if Map.member x' xs
-        then h xs x (succ n)
+        then h xs (succSplitVar x)
         else x'
   newVarH xs x =
-    if Map.member x xs then uncurry (h xs) (pullNum x) else x
+    if Map.member x xs then h xs (splitVar x) else x
 
 -- Alpha-rename a user-term
 renameUsTm :: UsTm -> RenameM UsTm
