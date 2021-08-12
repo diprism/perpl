e5eb8171c852f8f0a2f4681cd67dc7383cad4b36
Colin McDonald <colinmcd0731@gmail.com>
2021-08-12 18:49:33 -0400

    Add emacs mode with syntax highlighting

---
+++ ppl-emacs-mode.el
@@ -0,0 +1,50 @@
+(require 'generic-x)
+
+(defface ppl-keyword-face
+   '((((background light))
+      (:foreground "#8ac6f2" :weight bold))
+     (((background dark))
+      (:foreground "#8ac6f2" :weight bold)))
+   "Face for keywords"
+   :group 'ppl-highlight-faces)
+
+(defface ppl-dist-face
+   '((((background light))
+      (:foreground "darkgrey" :slant italic))
+     (((background dark))
+      (:foreground "lightgrey" :slant italic)))
+   "Face for fail, amb, and uniform"
+   :group 'ppl-highlight-faces)
+
+(defface ppl-punct-face
+   '((((background light))
+      (:foreground "dark slate blue"))
+     (((background dark))
+      (:foreground "plum")))
+   "Face for punctuation"
+   :group 'ppl-highlight-faces)
+
+(defface ppl-var-face
+   '((((background light))
+      (:foreground "#cae682"))
+     (((background dark))
+      (:foreground "#cae682")))
+   "Face for variables"
+   :group 'ppl-highlight-faces)
+
+(define-generic-mode 
+  'ppl-mode
+  '(("{-" . "-}"))
+  '()
+  '(("--.*" . 'font-lock-comment-face)
+    ("=\\|->\\|;\\|:\\|\\\\\\||" . 'ppl-punct-face)
+    ("\\(^\\|[^A-Za-z0-9\\'_]\\)\\(define\\|extern\\|data\\|case\\|of\\|let\\|in\\|sample\\)\\(^\\|[^A-Za-z0-9\\'_]\\)" 2 'ppl-keyword-face)
+    ("\\b\\(amb\\|fail\\|uniform\\)\\b" 1 'ppl-dist-face)
+    ("\\b\\([A-Za-z0-9\\'_]+\\)\\b" 1 'ppl-var-face)
+    )
+  '("\\.ppl$")
+  nil
+  "Basic highlighting mode for .ppl files"
+)
+
+(provide 'ppl-emacs-mode)
