2f4283453d04836a54064ce2342dce807583846c
Colin McDonald <colinmcd0731@gmail.com>
2021-08-13 11:08:55 -0400

    Code cleanup

--- Compile.hs
+++ Compile.hs
@@ -46,6 +46,7 @@ bindCases :: [External] -> [RuleM] -> RuleM
 bindCases xs =
   setExts xs . foldr (\ rm rm' -> rm +> {-resetExts-} rm') returnRule
 
+-- Creates dangling edges that discard a set of nodes
 discardEdges :: [Var] -> [Int] -> [Int] -> [Edge]
 discardEdges xs i_xs i_ns = map (\ (x, i_x, i_n) -> Edge [i_x, i_n] x) (zip3 xs i_xs i_ns)
 
@@ -55,6 +56,9 @@ newName i = " " ++ show i
 newNames :: Int -> [a] -> [Var]
 newNames i as = map newName [i..length as - 1 + i]
 
+newNames' :: [a] -> [(Var, a)]
+newNames' as = zip (newNames 0 as) as
+
 -- Add rule for a term application
 tmapp2fgg :: Ctxt -> Term -> RuleM
 tmapp2fgg g (TmApp tm1 tm2 tp2 tp) =
@@ -62,8 +66,7 @@ tmapp2fgg g (TmApp tm1 tm2 tp2 tp) =
   term2fgg g tm2 +>= \ xs2 ->
   let fac = pairFactorName tp2 tp
       (ns, [[itp2, itp, iarr], ixs1, ixs2]) =
-        -- For " 0" et al, pick impossible var names
-        combineExts [[(" 0", tp2), (" 1", tp), (" 2", TpArr tp2 tp)], xs1, xs2]
+        combineExts [newNames' [tp2, tp, TpArr tp2 tp], xs1, xs2]
       es = [Edge (ixs2 ++ [itp2]) (show tm2),
             Edge (ixs1 ++ [iarr]) (show tm1),
             Edge [itp2, itp, iarr] fac]
@@ -76,7 +79,7 @@ ctorRules :: Ctxt -> Ctor -> Type -> [Ctor] -> RuleM
 ctorRules g (Ctor x as) y cs =
   let ix = foldr (\ (Ctor x' _) next ix -> if x == x' then ix else next (ix + 1)) id cs 0
       as' = map (\ (i, a) -> (etaName x i, a)) (enumerate as)
-      (ns, [ias, [iy]]) = combineExts [as', [(" 0", y)]]
+      (ns, [ias, [iy]]) = combineExts [as', newNames' [y]]
       fac = ctorFactorName x (paramsToArgs as') y
       es = [Edge (ias ++ [iy]) fac]
       xs = ias ++ [iy]
@@ -103,7 +106,7 @@ caseRule g all_fvs xs_ctm ctm y cs tp (Case x as xtm) =
       fac = ctorFactorName x (paramsToArgs (nameParams x (map snd as))) (TpVar y)
       
       (ns, [[ictm, ixtm], ixs_xtm_as, ixs_as, ixs_ctm, all_ixs, d_ixs, d_ins]) =
-        combineExts [[(" 0", TpVar y), (" 1", tp)], xs_xtm_as, as, xs_ctm, all_xs, zip d_xs d_tps, zip d_ns d_tps]
+        combineExts [newNames' [TpVar y, tp], xs_xtm_as, as, xs_ctm, all_xs, zip d_xs d_tps, zip d_ns d_tps]
       (ixs_xtm, ixs_as') = foldr (\ (a, i) (ixs_xtm, ixs_as) -> if elem (fst a) (map fst as) then (ixs_xtm, (fst a, i) : ixs_as) else (i : ixs_xtm, ixs_as)) ([], []) (zip xs_xtm_as ixs_xtm_as)
       es = Edge (ixs_ctm ++ [ictm]) (show ctm) :
            Edge (ixs_xtm_as ++ [ixtm]) (show xtm) :
@@ -130,7 +133,7 @@ lamRule :: Bool -> Var -> Type -> Term -> Type -> RuleM -> RuleM
 lamRule addVarRule x tp tm tp' rm = -- TODO: new discard rule stuff?
   bindExt addVarRule x tp $
   rm +>= \ tmxs ->
-  let (ns, [[itp, itp', iarr], ixs]) = combineExts [[(x, tp), (" 1", tp'), (" 2", TpArr tp tp')], tmxs]
+  let (ns, [[itp], [itp', iarr], ixs]) = combineExts [[(x, tp)], newNames' [tp', TpArr tp tp'], tmxs]
       ixs' = delete itp ixs
       es = [Edge (ixs ++ [itp']) (show tm),
             Edge [itp, itp', iarr] (pairFactorName tp tp')]
@@ -148,7 +151,7 @@ term2fgg g (TmVarG gv x as y) =
   map (\ (a, atp) -> term2fgg g a) (reverse as) +*>= \ xss' ->
   -- TODO: instead of reversing, just have (+*>=) do that
   let xss = reverse xss'
-      (ns, [iy] : ias : ixss) = combineExts ([(" 0", y)] : map (\ (i, (tm, tp)) -> (' ' : show (succ i), tp)) (enumerate as) : xss)
+      (ns, [iy] : ias : ixss) = combineExts (newNames' [y] : map (\ (i, (tm, tp)) -> (' ' : show (succ i), tp)) (enumerate as) : xss)
       es_c = Edge (ias ++ [iy]) (ctorFactorNameDefault x (map snd as) y) :
                  map (\ (ixs, (a, _), itp) -> Edge (ixs ++ [itp]) (show a))
                      (zip3 ixss as ias)
@@ -182,7 +185,8 @@ term2fgg g (TmLet x xtm xtp tm tp) =
   term2fgg g xtm +>= \ xtmxs ->
   bindExt True x xtp $
   term2fgg (ctxtDeclTerm g x xtp) tm +>= \ tmxs ->
-  let (ns, [[ixtp, itp], ixxs, ixs]) = combineExts [[(x, xtp), (" 0", tp)], xtmxs, tmxs]
+  let (ns, [[ixtp], [itp], ixxs, ixs]) =
+        combineExts [[(x, xtp)], newNames' [tp], xtmxs, tmxs]
       ixs' = delete ixtp ixs
       es = [Edge (ixxs ++ [ixtp]) (show xtm), Edge (ixs ++ [itp]) (show tm)]
       xs = nub (ixxs ++ ixs') ++ [itp]
@@ -195,13 +199,13 @@ prog2fgg g (ProgFun x ps tm tp) = -- TODO: add factor for joinArrows ps tp
   bindExts True ps $ term2fgg (ctxtDeclArgs g ps) tm +>= \ tmxs ->
   let (unused_x, unused_tp) = unzip (Map.toList (Map.difference (Map.fromList ps) (Map.fromList tmxs)))
       unused_n = map (\ i -> " " ++ show (i + 1)) [0..length unused_x - 1]
-      (ns, [[itp], ixs, ips, un_n_ixs, un_x_ixs]) = combineExts [[(" 0", tp)], tmxs, ps, zip unused_n unused_tp, zip unused_x unused_tp]
+      (ns, [[itp], ixs, ips, un_n_ixs, un_x_ixs]) = combineExts [newNames' [tp], tmxs, ps, zip unused_n unused_tp, zip unused_x unused_tp]
       es = Edge (ixs ++ [itp]) (show tm) : discardEdges unused_x un_x_ixs un_n_ixs
       xs = ips ++ [itp]
   in
     addRule' (TmVarG DefVar x [] tp) (map snd ns) es xs
 prog2fgg g (ProgExtern x xp ps tp) =
-  let (ns, [(itp : ixs)]) = combineExts [[(" " ++ show i, atp) | (i, atp) <- enumerate (tp : ps)]]
+  let (ns, [(itp : ixs)]) = combineExts [newNames' (tp : ps)]
       es = [Edge (ixs ++ [itp]) xp]
       xs = ixs ++ [itp]
       ws = getExternWeights (domainValues g) ps tp
