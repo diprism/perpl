f5c1257d26a2be64b365362aad78918dd8cc2c9e
David Chiang <dchiang@nd.edu>
2022-07-04 13:29:41 -0700

    fix makefile

--- run_tests.sh
+++ run_tests.sh
@@ -2,11 +2,25 @@
 
 my_errs=()
 
-for file in code/{amb,datatype_polymorphism,derefun,double,dyck,equal,example12,extinction,fsm,fsm2,function_polymorphism,head_tail_rec,pattern1,pattern2,pda,pda2,penney,products,reverse,sample,stairs,syntax,tree,von_neumann}.ppl
+for file in examples/{amb,datatype_polymorphism,derefun,double,dyck,equal,example12,extinction,fsm,fsm2,function_polymorphism,head_tail_rec,pattern1,pattern2,pda,pda2,penney,products,reverse,sample,stairs,syntax,tree,von_neumann}.ppl
 do
     printf '%-40s' "Compiling ${file}... "
     my_err=$(./compiler.exe $file -o /dev/null 2>&1 > /dev/null)
     if [ $? = 0 ]
+    then
+        echo "Success"
+    else
+        echo "Failure"
+        my_errs+="Error in ${file}: ${my_err}
+"
+    fi
+done
+
+for file in tests/bad/{case_of_zero_cases.ppl,data_with_zero_cons}.ppl
+do
+    printf '%-40s' "Compiling ${file}... "
+    my_err=$(./compiler.exe $file -o /dev/null 2>&1 > /dev/null)
+    if [ $? != 0 ]
     then
         echo "Success"
     else
