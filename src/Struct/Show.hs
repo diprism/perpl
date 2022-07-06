c57773ae0ba8eb827e1e7baabc03b9c33c2cb6ff
David Chiang <dchiang@nd.edu>
2022-07-06 11:57:37 -0700

    Partial support for 0-ary additive products (<>). These are needed when refunctionalizing a type that has no unfolds. The support is only partial because there's no way to write the type of <>.

--- src/Struct/Show.hs
+++ src/Struct/Show.hs
@@ -117,6 +117,7 @@ showTypeh :: Type -> String
 showTypeh (TpVar y as) = delimitWith " " (y : [showType a ShowTypeArg | a <- as])
 showTypeh (TpArr tp1 tp2) = showType tp1 ShowArrL ++ " -> " ++ showType tp2 ShowNone
 showTypeh (TpProd Multiplicative []) = "Unit"
+showTypeh (TpProd Additive []) = "<>" -- this is not a legal type, but the output FGG needs to have something
 showTypeh (TpProd am tps) = delimitWith (if am == Additive then " & " else " * ") [showType tp ShowTypeArg | tp <- tps]
 showTypeh NoTp = ""
 
