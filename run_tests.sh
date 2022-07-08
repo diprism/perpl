3a3eb9e08fd9165db7344658a3e3cac6cdf3828e
David Chiang <dchiang@nd.edu>
2022-07-08 09:15:13 -0700

    add test against polymorphic recursion

--- run_tests.sh
+++ run_tests.sh
@@ -33,7 +33,7 @@ do
     fi
 done
 
-for file in tests/bad/{case_of_zero_cases.ppl,case_without_bar,data_with_zero_cons,data_without_bar}.ppl
+for file in tests/bad/{case_of_zero_cases.ppl,case_without_bar,data_with_zero_cons,data_without_bar,polymorphic_recursion}.ppl
 do
     printf '%-40s' "Compiling ${file}... "
     my_err=$(./compiler.exe $file -o /dev/null 2>&1 > /dev/null)
