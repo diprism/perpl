b9265befb0a4737bf1342be4297d7fab534749bb
Chung-chieh Shan <ccshan@indiana.edu>
2022-07-17 02:56:19 +0800

    Revamp precedence treatment in term Showing
    Closes #67

--- src/Struct/Show.hs
+++ src/Struct/Show.hs
@@ -38,47 +38,14 @@ toUsProgs (Progs ps tm) = UsProgs (map toUsProg ps) (toUsTm tm)
 
 {- Show Instances -}
 
-data ShowHist = ShowAppL | ShowAppR | ShowEqs | ShowCase
-              | ShowArrL | ShowTypeArg
+data ShowHist = ShowArrL | ShowTypeArg
               | ShowNone
   deriving Eq
 
--- Should we add parens to this term, given its parent term?
-showTermParens :: UsTm -> ShowHist -> Bool
-showTermParens (UsLam _ _ _    ) ShowAppL = True
-showTermParens (UsLam _ _ _    ) ShowAppR = True
-showTermParens (UsLam _ _ _    ) ShowEqs  = True
-showTermParens (UsApp _ _      ) ShowAppR = True
-showTermParens (UsCase _ _     ) ShowAppL = True
-showTermParens (UsCase _ _     ) ShowAppR = True
-showTermParens (UsCase _ _     ) ShowCase = True
-showTermParens (UsCase _ _     ) ShowEqs  = True
-showTermParens (UsIf _ _ _     ) ShowAppL = True
-showTermParens (UsIf _ _ _     ) ShowAppR = True
-showTermParens (UsIf _ _ _     ) ShowEqs  = True
-showTermParens (UsEqs _        ) ShowAppL = True
-showTermParens (UsEqs _        ) ShowAppR = True
-showTermParens (UsEqs _        ) ShowEqs  = True
-showTermParens (UsLet _ _ _    ) ShowAppL = True
-showTermParens (UsLet _ _ _    ) ShowAppR = True
-showTermParens (UsLet _ _ _    ) ShowEqs  = True
-showTermParens (UsAmb _        ) ShowAppL = True
-showTermParens (UsAmb _        ) ShowAppR = True
-showTermParens (UsFactor _ _   ) ShowAppL = True
-showTermParens (UsFactor _ _   ) ShowAppR = True
-showTermParens (UsFail NoTp    ) ShowAppL = False
-showTermParens (UsFail _       ) ShowAppL = True
-showTermParens (UsFail _       ) ShowAppR = True
-showTermParens (UsFail _       ) ShowEqs  = True
-showTermParens (UsElimProd _ _ _ _) ShowAppL = True
-showTermParens (UsElimProd _ _ _ _) ShowAppR = True
-showTermParens (UsElimProd _ _ _ _) ShowEqs  = True
-showTermParens _                 _        = False
-
 -- Should we add parens to this type, given its parent type?
 showTypeParens :: Type -> ShowHist -> Bool
-showTypeParens (TpArr _ _) ShowArrL = True
 showTypeParens (TpArr _ _) ShowTypeArg = True
+showTypeParens (TpArr _ _) ShowArrL = True
 showTypeParens (TpVar _ (_ : _)) ShowTypeArg = True
 showTypeParens _ _ = False
 
@@ -90,26 +57,6 @@ amParens :: AddMult -> (String, String)
 amParens Additive = ("<", ">")
 amParens Multiplicative = ("(", ")")
 
--- Term show helper (ignoring parentheses)
-showTermh :: UsTm -> String
-showTermh (UsVar x) = x
-showTermh (UsLam x tp tm) = "\\ " ++ x ++ showTpAnn tp ++ ". " ++ showTerm tm ShowNone
-showTermh (UsApp tm1 tm2) = showTerm tm1 ShowAppL ++ " " ++ showTerm tm2 ShowAppR
-showTermh (UsCase tm cs) = "case " ++ showTerm tm ShowNone ++ " of " ++ showCasesCtors cs
-showTermh (UsIf tm1 tm2 tm3) = "if " ++ showTerm tm1 ShowNone ++ " then " ++ showTerm tm2 ShowNone ++ " else " ++ showTerm tm3 ShowNone
-showTermh (UsTmBool b) = if b then "True" else "False"
-showTermh (UsLet x tm tm') = "let " ++ x ++ " = " ++ showTerm tm ShowNone ++ " in " ++ showTerm tm' ShowNone
-showTermh (UsAmb tms) = foldl (\ s tm -> s ++ " " ++ showTerm tm ShowAppR) "amb" tms
-showTermh (UsFactor wt tm) = "factor " ++ show wt ++ " in " ++ showTerm tm ShowNone
-showTermh (UsFail tp) = "fail" ++ showTpAnn tp
-showTermh (UsProd am tms) =
-  let (l, r) = amParens am in
-    l ++ intercalate ", " [showTerm tm ShowNone | tm <- tms] ++ r
-showTermh (UsElimProd am tm xs tm') =
-  let (l, r) = amParens am in
-    "let " ++ l ++ intercalate ", " xs ++ r ++ " = " ++ showTerm tm ShowNone ++ " in " ++ showTerm tm' ShowNone
-showTermh (UsEqs tms) = intercalate " == " [showTerm tm ShowEqs | tm <- tms]
-
 -- Type show helper (ignoring parentheses)
 showTypeh :: Type -> String
 showTypeh (TpVar y as) = intercalate " " (y : [showType a ShowTypeArg | a <- as])
@@ -117,10 +64,6 @@ showTypeh (TpArr tp1 tp2) = showType tp1 ShowArrL ++ " -> " ++ showType tp2 Show
 showTypeh (TpProd am tps) = let (l, r) = amParens am in
   l ++ intercalate ", " [showType tp ShowNone | tp <- tps] ++ r
 showTypeh NoTp = ""
-
--- Show a term, given its parent for parentheses
-showTerm :: UsTm -> ShowHist -> String
-showTerm tm h = parensIf (showTermParens tm h) (showTermh tm)
 
 -- Show a type, given its parent for parentheses
 showType :: Type -> ShowHist -> String
@@ -134,17 +77,29 @@ showCasesCtors (c : cs) = show c ++ " | " ++ showCasesCtors cs
 
 -- Actual show instances
 instance Show CaseUs where
-  show (CaseUs x as tm) = foldl (\ x a -> x ++ " " ++ a) x as ++ " -> " ++ showTerm tm ShowCase
+  showsPrec p (CaseUs x as tm) = delimitWith " " (map showString (x:as)) . showString " -> " . showsPrec p tm
 instance Show Case where
-  show = show . toCaseUs
+  showsPrec p = showsPrec p . toCaseUs
 
 instance Show Ctor where
   show (Ctor x as) = foldl (\ x a -> x ++ " " ++ showType a ShowTypeArg) x as
 
 instance Show UsTm where
-  show = flip showTerm ShowNone
+  showsPrec _ (UsVar x) = showString x
+  showsPrec p (UsApp tm1 tm2) = showParen (p > 10) (showsPrec 10 tm1 . showChar ' ' . showsPrec 11 tm2)
+  showsPrec p (UsLam x tp tm) = showParen (p > 1) (showString "\\ " . showString x . showString (showTpAnn tp) . showString ". " . showsPrec (if p == 1 then 1 else 0) tm)
+  showsPrec p (UsLet x tm tm') = showParen (p > 1) (showString "let " . showString x . showString " = " . shows tm . showString " in " . showsPrec (if p == 1 then 1 else 0) tm')
+  showsPrec p (UsElimProd am tm xs tm') = showParen (p > 1) (let (l, r) = amParens am in showString "let " . showString l . delimitWith ", " (map showString xs) . showString r . showString " = " . shows tm . showString " in " . showsPrec (if p == 1 then 1 else 0) tm')
+  showsPrec p (UsFactor wt tm) = showParen (p > 1) (showString "factor " . shows wt . showString " in " . showsPrec (if p == 1 then 1 else 0) tm)
+  showsPrec p (UsIf tm1 tm2 tm3) = showParen (p > 1) (showString "if " . shows tm1 . showString " then " . shows tm2 . showString " else " . showsPrec (if p == 1 then 1 else 0) tm3)
+  showsPrec p (UsCase tm cs) = showParen (p > 0) (showString "case " . shows tm . showString " of " . delimitWith " | " (map (showsPrec 1) cs))
+  showsPrec p (UsAmb tms) = showParen (p > 9) (delimitWith " " (showString "amb" : map (showsPrec 11) tms))
+  showsPrec p (UsFail tp) = showParen (tp /= NoTp && p > 1) (showString "fail" . showString (showTpAnn tp))
+  showsPrec _ (UsProd am tms) = let (l, r) = amParens am in showString l . delimitWith ", " (map shows tms) . showString r
+  showsPrec _ (UsTmBool b) = showString (if b then "True" else "False")
+  showsPrec p (UsEqs tms) = showParen (p > 4) (delimitWith " == " (map (showsPrec 5) tms))
 instance Show Term where
-  show = show . toUsTm
+  showsPrec p = showsPrec p . toUsTm
 
 instance Show Type where
   show = flip showType ShowNone
