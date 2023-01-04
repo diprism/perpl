3a6ebe289bddbb3bc9cadbe779d97b3b4b7f8c0b
Colin McDonald <colinmcd0731@gmail.com>
2023-01-04 10:50:24 -0500

    Tweak emacs mode

--- ppl-emacs-mode.el
+++ ppl-emacs-mode.el
@@ -7,7 +7,7 @@
   '(("--.*" . 'font-lock-comment-face)
     ("\\b\\([0-9][0-9]*\\(\\.[0-9]+\\)?\\)\\b" 1 'font-lock-builtin-face)
     ("->\\|=\\|&\\|\\*\\|;\\|:\\|\\.\\|,\\|\\\\\\||" . 'font-lock-variable-name-face)
-    ("data\\|amb\\|factor\\|fail\\|define\\|extern\\|case\\|of\\|let\\|in\\|if\\|then\\|else" . 'font-lock-keyword-face)
+    ("\\bdata\\b\\|\\bamb\\b\\|\\bfactor\\b\\|\\bfail\\b\\|\\bdefine\\b\\|\\bextern\\b\\|\\bcase\\b\\|\\bof\\b\\|\\blet\\b\\|\\bin\\b\\|\\bif\\b\\|\\bthen\\b\\|\\belse\\b" . 'font-lock-keyword-face)
     )
   '("\\.ppl$")
   nil
